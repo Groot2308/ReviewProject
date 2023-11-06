7e6f9ed6ab3e03f8bfd3651154b680496e9fc7d0
Groot2308 <danghoan2382002@gmail.com>
2023-11-06 20:37:40 +0700

    new

--- ProjectReview/Controllers/LocationController.cs
+++ ProjectReview/Controllers/LocationController.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjectReview.Models;
+using ProjectReview.Repositories;
 
 namespace ProjectReview.Controllers
 {
     public class LocationController : Controller
     {
+
+        private readonly ILocationRepository _locationRepository;
+        public LocationController(ILocationRepository locationRepository)
+        {
+
+            _locationRepository = locationRepository;
+        }
+
         public IActionResult Location()
         {
             return View();
+        }
+
+        public IActionResult LocationDetail(int id)
+        {
+            Location location = _locationRepository.GetSingleByCondition(l => l.Id == id);
+            return View(location); 
+
         }
     }
 }
